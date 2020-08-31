89c84b1c3a7544df0e69a04d39664543d6dfb03a
Thad Cole <thadjcole@gmail.com>
2020-08-31 11:38:40 -0400

    add team model

---
+++ models/team.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const Team = sequelize.definte("Team", {
+    name: {
+      type: DataTypes.STRING, // "Chiefs"
+    },
+    location: {
+      type: DataTypes.STRING, // "Kansas City"
+    },
+    abbr: {
+      type: DataTypes.STRING(3), // "KCC"
+    },
+    color1: {
+      type: DataTypes.STRING(7), // "#e31837" aka red
+    },
+    color2: {
+      type: DataTypes.STRING(7), // "#ffb81c" aka gold
+    },
+    picked: {
+      // will change to true when picked & win
+      // picked and lose keeps the team in play for future weeks
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  });
+
+  return Team;
+};
