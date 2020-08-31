8b434cba0506c19657084bf19d617dc1ec5303f6
Thad Cole <thadjcole@gmail.com>
2020-08-31 15:15:07 -0400

    travis config

--- config/config.js
+++ config/config.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
 module.exports = {
-  "development": {
-    "username": process.env.LOCALDB_USERNAME,
-    "database": process.env.LOCALDB_DATABASE,
-    "host": process.env.LOCALDB_HOST,
-    "dialect": "mysql"
+  development: {
+    username: process.env.LOCALDB_USERNAME,
+    database: process.env.LOCALDB_DATABASE,
+    host: process.env.LOCALDB_HOST,
+    dialect: "mysql",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
-  }
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql",
+  },
 };
