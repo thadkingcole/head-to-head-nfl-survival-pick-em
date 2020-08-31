de17ce85c47935ac98a2c57aef44b465d7e67a1f
Thad Cole <thadjcole@gmail.com>
2020-08-31 16:34:39 -0400

    add passport authentication

---
+++ config/passport.js
@@ -0,0 +1,28 @@
+// * required modules
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const db = require("../models");
+
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    db.User.findOne({ username: username }, (err, user) => {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+
+// In order to help keep authentication state across HTTP requests,
+// Sequelize needs to serialize and deserialize the user
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
+
+// Exporting our configured passport
+module.exports = passport;
--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-// required modules
+// * required modules
 const express = require("express"); // app instance
 const db = require("./models"); // database models
+const passport = require("./config/passport"); // our configured passport module
+const session = require("express-session"); // keep track of user login status
 
 // PORT designation
 const PORT = process.env.PORT || 3220; // number of NFL teams & current year
 
 // * create app instance
 const app = express();
-
-// * configure express app
+// configure express app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // * required routes
 require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
 
 // * syncing database & logging message to the user upon success
 // ! alter will update tables to match the models
