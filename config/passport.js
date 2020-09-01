55ca24c4166eed6c2b06f5790ba130164183cda3
Thad Cole <thadjcole@gmail.com>
2020-08-31 22:54:45 -0400

    can add user to db, but can't login

--- config/passport.js
+++ config/passport.js
@@ -3,16 +3,22 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const db = require("../models");
 
-passport.use(new LocalStrategy(
-  (username, password, done) => {
-    db.User.findOne({ username: username }, (err, user) => {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!user.verifyPassword(password)) { return done(null, false); }
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    db.User.findOne({ where: { username: username } }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+      if (!user.verifyPassword(password)) {
+        return done(null, false);
+      }
       return done(null, user);
     });
-  }
-));
+  })
+);
 
 // In order to help keep authentication state across HTTP requests,
 // Sequelize needs to serialize and deserialize the user
