0714ec9d311be23715de5afecac833138712e926
Thad Cole <thadjcole@gmail.com>
2020-11-01 09:11:04 -0500

    start development with react & bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Jumbotron>
+      <Container>
+        <h1>2 player NFL survival pick'em</h1>
+        <p>NFL survival pick'em... with a twist!</p>
+        <p>Currently under construction. Stay tuned!</p>
+      </Container>
+    </Jumbotron>
   );
 }
 
