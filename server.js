c1520b7a13d39592547a66f04f1ef3c1af36e782
Thad Cole <thadjcole@gmail.com>
2020-11-09 20:46:04 -0500

    fill out back-end server code

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 // * required modules
 const express = require("express"); // app instance
 const session = require("express-session"); // keep track of user login status
+const mongoose = require("mongoose"); // mongoDB orm
+const MongoStore = require("connect-mongo")(session);
+const passport = require("passport");
+require("dotenv").config();
+
+const mongooseConnection = require("./db");
+
+// TODO: develop backend routes
+// const routes = require("/.routes");
 
 // PORT designation
 const PORT = process.env.PORT || 3220; // number of NFL teams & current year
@@ -11,8 +20,31 @@ const app = express();
 // configure express app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
+// Serve up static assets (usually on heroku)
+process.env.NODE_ENV === "production" &&
+  app.use(express.static("client/build"));
+
+// Sessions
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    store: new MongoStore({
+      mongooseConnection,
+    }),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// TODO: Add routes
+// app.use(routes);
+
+// Start the API server
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
