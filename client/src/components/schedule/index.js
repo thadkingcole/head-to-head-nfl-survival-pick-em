80930f0624df0da6f5c9dcf771fdcf6dc5eba2b7
Thad Cole <thadjcole@gmail.com>
2020-11-21 11:11:14 -0500

    week games table made symmetrical; fix unique key warnings

--- client/src/components/schedule/index.js
+++ client/src/components/schedule/index.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import Table from "react-bootstrap/Table";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -46,25 +46,29 @@ function Schedule() {
           <h2>Week {weekNum}</h2>
         </Col>
         <Col className="text-right">
-          <label for="weekNum">Show Week:</label>
+          <label>Show Week:</label>
           <select name="weekNum" id="weekNum" onChange={handleChange}>
             {weekList.map((n) => {
               if (weekNum === n) {
                 return (
-                  <option value={n} selected>
+                  <option key={n} value={n} selected>
                     {n}
                   </option>
                 );
               }
-              return <option value={n}>{n}</option>;
+              return (
+                <option key={n} value={n}>
+                  {n}
+                </option>
+              );
             })}
           </select>
         </Col>
       </Row>
       <Row>
-        <Table size="sm">
+        <Table size="sm" className="text-center">
           <thead>
-            <tr className="text-nowrap">
+            <tr className="text-center">
               <th colSpan="2">Away</th>
               <th colSpan="2">Home</th>
               <th>Status</th>
@@ -75,19 +79,16 @@ function Schedule() {
               return (
                 <tr key={game.uid}>
                   {game.competitions[0].competitors
-                    // TODO: format table such that:
-                    // away team ... away score | home score ... home team
-
                     // shallow copy to display away then home
                     .slice(0)
                     .reverse()
-                    .map((team) => {
+                    .map((team, i) => {
                       const teamStyle = {
                         backgroundColor: `#${team.team.color}`,
                         color: "white",
                       };
-                      return (
-                        <>
+                      return i === 0 ? (
+                        <Fragment key={team.team.uid}>
                           <td style={teamStyle}>
                             <img
                               src={team.team.logo}
@@ -97,12 +98,29 @@ function Schedule() {
                             />
                             {team.team.displayName}
                           </td>
-                          <td style={teamStyle} className="text-center">
+                          <td style={teamStyle}>
                             {game.status.type.id >= 2
                               ? team.score
                               : team.records[0].summary}
                           </td>
-                        </>
+                        </Fragment>
+                      ) : (
+                        <Fragment key={team.team.uid}>
+                          <td style={teamStyle}>
+                            {game.status.type.id >= 2
+                              ? team.score
+                              : team.records[0].summary}
+                          </td>
+                          <td style={teamStyle}>
+                            <img
+                              src={team.team.logo}
+                              alt={team.team.abbreviation}
+                              width="25"
+                              className="float-right"
+                            />
+                            {team.team.displayName}
+                          </td>
+                        </Fragment>
                       );
                     })}
                   {(() => {
